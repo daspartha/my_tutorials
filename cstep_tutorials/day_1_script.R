8abc801ca7142595f438ea179b835b21eea07f63
daspartha <partha67@gmail.com>
2020-01-24 03:02:34 +0530

    tidyr dplyr tutorial

--- cstep_tutorials/day_1_script.R
+++ cstep_tutorials/day_1_script.R
@@ -1,3 +1,19 @@
 
 library(tidyverse)
-library(nycflights13)
+library(here)
+
+
+
+# data
+
+netflix <- read_csv(here('/cstep_tutorials/data', 'netflix-shows.zip'), col_types = 
+                      cols(date_added = col_date(format = "%B %d, %Y")))
+
+# 
+
+netflix %>% 
+  mutate(date_added = mdy(date_added),
+         release_year = ymd(release_year))
+
+
+parse_date("November 30, 2019", "%M %d %Y")
